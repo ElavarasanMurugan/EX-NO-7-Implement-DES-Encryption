edf0bcf97209d5ebb7da7956f8ce6fe1e4cba2ec
ElavarasanMurugan <elavarasanmurugan2407@gmail.com>
2025-09-19 09:31:47 +0530

    Success

--- des.c
+++ des.c
@@ -1,19 +1,3 @@
-# EX-NO-7-Implement-DES-Encryption
-
-## Aim:
-
-To use the Data Encryption Standard (DES) algorithm for a practical application, such as securing sensitive data transmission in financial transactions.
-
-## ALGORITHM:
-
-1. DES is based on a symmetric key encryption technique that encrypts data in 64-bit blocks.
-2. DES uses a Feistel network structure with 16 rounds of processing for encryption.
-3. DES has a 64-bit key, but only 56 bits are used for encryption (the remaining 8 bits are for parity).
-4. DES applies initial and final permutations along with 16 rounds of substitution and permutation transformations to produce ciphertext.
-
-## Program:
-
-```c
 #include <stdio.h>
 #include <string.h>
 
@@ -83,14 +67,3 @@ int main()
 
     return 0;
 }
-
-```
-
-
-## Output:
-
-![alt text](DES.png)
-
-## Result:
-  The program is executed successfully
-
